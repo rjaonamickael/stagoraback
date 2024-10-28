cb426a9242d78600253f571d42ece1c8d97eb7bd
rjaonamickael <r.jaonamickael@gmail.com>
2024-10-28 17:41:08 -0400

    Premier commit

---
+++ Stagoraback/src/test/java/com/stagora/StagorabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stagora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagorabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
