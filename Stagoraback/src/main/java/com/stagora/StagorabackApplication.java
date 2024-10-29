3906f7a73516b07cded916eab4d7506ca5e1912f
rjaonamickael <r.jaonamickael@fmail.com>
2024-10-28 21:41:44 -0400

    Entitées Etablissement créer.

--- Stagoraback/src/main/java/com/stagora/StagorabackApplication.java
+++ Stagoraback/src/main/java/com/stagora/StagorabackApplication.java
@@ -3,11 +3,14 @@ package com.stagora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class StagorabackApplication {
+public class StagorabackApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StagorabackApplication.class, args);
 	}
+
 
 }
