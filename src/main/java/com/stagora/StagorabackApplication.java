a17e781bd35b29dd711ea883364bb32cb4278051
Rachid <webbsense@gmail.com>
2024-11-13 15:52:10 -0500

    Initial commit for Spring Boot backend

---
+++ src/main/java/com/stagora/StagorabackApplication.java
@@ -0,0 +1,13 @@
+package com.stagora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagorabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagorabackApplication.class, args);
+	}
+
+}
