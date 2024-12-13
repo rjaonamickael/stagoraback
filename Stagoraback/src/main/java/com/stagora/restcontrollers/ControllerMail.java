45c5973b44ea3d72e6bf6f7cd695f7b86bfdf079
ksimali <cassim.ali@outlook.fr>
2024-12-13 09:57:06 -0500

    Update ControllerMail.java :
    - Update try catch exception

--- Stagoraback/src/main/java/com/stagora/restcontrollers/ControllerMail.java
+++ Stagoraback/src/main/java/com/stagora/restcontrollers/ControllerMail.java
@@ -1,6 +1,11 @@
 package com.stagora.restcontrollers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.stagora.services.ServiceMailing;
@@ -8,6 +13,7 @@ import com.stagora.utils.user.EmailRequest;
 
 @RestController
 @RequestMapping("/mail")
+@CrossOrigin(origins = "http://localhost:4200") 
 public class ControllerMail {
 
     @Autowired
@@ -15,13 +21,19 @@ public class ControllerMail {
     
 
     @PostMapping("/send")
-    public String sendCustomMail(@RequestBody EmailRequest emailRequest) {
-    	try {
-    		serviceMailing.sendCustomEmail(emailRequest.getEmail(), emailRequest.getSubject(), emailRequest.getContenu());
-            return "Email personnalisé envoyé avec succès.";
+    public ResponseEntity<Map<String, Object>> sendCustomMail(@RequestBody EmailRequest emailRequest) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            serviceMailing.sendCustomEmail(emailRequest.getEmail(), emailRequest.getSubject(), emailRequest.getContenu());
+            response.put("ok", true);
+            response.put("message", "Email personnalisé envoyé avec succès.");
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
-            return "Erreur lors de l'envoi de l'email.";
+            response.put("ok", false);
+            response.put("message", "Erreur lors de l'envoi de l'email.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
+
     
 }
