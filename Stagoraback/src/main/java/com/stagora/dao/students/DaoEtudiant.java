e76fe19e3b3c9080306f00f39d1ce2d0c0aee5a4
Mickaël RATISARIJAONA <r.jaonamickael@gmail.com>
2024-12-12 21:23:34 -0500

    merge travail driss

--- Stagoraback/src/main/java/com/stagora/dao/students/DaoEtudiant.java
+++ Stagoraback/src/main/java/com/stagora/dao/students/DaoEtudiant.java
@@ -1,11 +1,14 @@
 package com.stagora.dao.students;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+
 import com.stagora.entities.students.Etudiant;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface DaoEtudiant extends JpaRepository<Etudiant, Long> {
+public interface DaoEtudiant extends JpaRepository<Etudiant, Long>, JpaSpecificationExecutor<Etudiant> {
     // Méthode pour trouver un étudiant via son userId
     @Query("SELECT e FROM Etudiant e WHERE e.user.id = :userId")
     Etudiant findByUserId(@Param("userId") Long userId);
